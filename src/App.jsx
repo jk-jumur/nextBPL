6d45311bfd4a96540b9982b0ae6622967609b536
jk-jumur <developer.jumur@gmail.com>
2026-05-24 00:06:26 +0600

    nextBPL project set up

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+
+      <h2 className=' text-red-400 text-2xl'>Hello React</h2>
+
+        
+
+         
+    </>
+  )
+}
+
+export default App
